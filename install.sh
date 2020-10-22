912d457a8165a63ef0403084fc49880e839e0d65
YoNyeoSeok <yys8646@postech.ac.kr>
2020-10-23 04:03:42 +0900

    UPDAE to use source bashrc

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ URL=https://github.com/YoNyeoSeok/dotfiles.git
 sudo apt-get install -y git vim screen
 
 git clone --bare $URL ~/.dotfiles.git
-echo .dotfiles.git >> $HOME/.gitignore
-git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME/ config --local status.showUntrackedFiles no
+echo .dotfiles.git >> ~/.gitignore
 echo alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME/" >> $HOME/.bash_aliases
+source ~/.bashrc
+dotfiles config --local status.showUntrackedFiles no
