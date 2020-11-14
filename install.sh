44a01a38669fa85e3b9846abdcef147f17bf9b25
YoNyeoSeok <yys8646@postech.ac.kr>
2020-11-14 08:21:15 +0000

    UPDATE bash aliases string

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ sudo apt-get install -y git vim screen
 
 git clone --bare $URL ~/.dotfiles.git
 echo .dotfiles.git >> ~/.gitignore
-echo alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME/" >> $HOME/.bash_aliases
+echo "alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME/'" >> $HOME/.bash_aliases
 source ~/.bashrc
 dotfiles config --local status.showUntrackedFiles no
